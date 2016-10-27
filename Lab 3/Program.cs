a6cb9367c20dabd9c905cf38bb5786858aa033dd
zuralldoe <quirico@gmail.com>
2016-10-27 11:47:49 -0400

    adding Lab3

---
+++ Lab 3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void SquaresandCubes()
+        {
+            int UserInput = Convert.ToInt32(Console.ReadLine());
+            int Square;
+            int Cube;
+
+            for (int i = 0; i <= UserInput; i++)
+            {
+                Square = (i * i);
+                Cube = (i * i * i);
+
+                Console.WriteLine(i + "          " + Square + "          " + Cube);
+
+            }
+        
+          
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Learn your squares and cubes!\n");
+
+            string run;
+            do
+            {
+                Console.WriteLine("\nEnter an intiger:");
+               
+                SquaresandCubes();
+
+                Console.WriteLine("Do you want to Continue?");
+                run = Console.ReadLine();
+            }
+            while (run == "y");    
+            }
+        }
+}
